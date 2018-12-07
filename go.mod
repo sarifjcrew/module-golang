23c73ef694fd34ac653bfa4685a1911a439a3e08
sarifuddin <sarifjcrew@gmail.com>
2018-12-07 09:42:19 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/sarifjcrew/module-golang/lib
---
+++ lib.go
@@ -0,0 +1,10 @@
+package lib
+
+import (
+	"fmt"
+)
+
+// SayHello used for saying Hello to you
+func SayHello() {
+	fmt.Println("HEllo...")
+}
